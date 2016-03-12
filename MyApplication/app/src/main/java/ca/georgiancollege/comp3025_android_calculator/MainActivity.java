6a06be941446f99c418a073a7923d06bfb127d11
BlaineParr <blaine.parr@hotmail.com>
2016-03-11 21:03:29 -0500

    Revert "Added some code."
    This reverts commit b8da5686dced064a89c25142516db169a3d7ceb8.

--- MyApplication/app/src/main/java/ca/georgiancollege/comp3025_android_calculator/MainActivity.java
+++ MyApplication/app/src/main/java/ca/georgiancollege/comp3025_android_calculator/MainActivity.java
@@ -5,43 +5,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    //instance variables
-    private double total = 0;
-    private String output = "";
-
-    final Button clearButton = (Button) findViewById(R.id.clearButton);
-    final Button posNegButton = (Button) findViewById(R.id.posNegButton);
-    final Button percentButton = (Button) findViewById(R.id.percentButton);
-    final Button divideButton = (Button) findViewById(R.id.divideButton);
-    final Button sevenButton = (Button) findViewById(R.id.sevenButton);
-    final Button eightButton = (Button) findViewById(R.id.eightButton);
-    final Button nineButton = (Button) findViewById(R.id.nineButton);
-    final Button multiplyButton = (Button) findViewById(R.id.multiplyButton);
-    final Button fourButton = (Button) findViewById(R.id.fourButton);
-    final Button fiveButton = (Button) findViewById(R.id.fiveButton);
-    final Button sixButton = (Button) findViewById(R.id.sixButton);
-    final Button addButton = (Button) findViewById(R.id.addButton);
-    final Button oneButton = (Button) findViewById(R.id.oneButton);
-    final Button twoButton = (Button) findViewById(R.id.twoButton);
-    final Button threeButton = (Button) findViewById(R.id.threeButton);
-    final Button subtractButton = (Button) findViewById(R.id.subtractButton);
-    final Button zeroButton = (Button) findViewById(R.id.zeroButton);
-    final Button decimalButton = (Button) findViewById(R.id.decimalButton);
-    final Button calculateButton = (Button) findViewById(R.id.calculateButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
@@ -67,6 +40,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-
-} //class MainActivity ends
+}
