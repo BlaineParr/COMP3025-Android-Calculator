13fd67594c1739b23457655bf55ff7fea8f11157
BlaineParr <blaine.parr@hotmail.com>
2016-03-11 21:19:31 -0500

    Added some working code.

--- MyApplication/app/src/main/java/ca/georgiancollege/comp3025_android_calculator/MainActivity.java
+++ MyApplication/app/src/main/java/ca/georgiancollege/comp3025_android_calculator/MainActivity.java
@@ -8,8 +8,32 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //instance variables
+    private TextView outputTextView;
+    private Button clearButton;
+    private Button posNegButton;
+    private Button percentButton;
+    private Button divideButton;
+    private Button sevenButton;
+    private Button eightButton;
+    private Button nineButton;
+    private Button multiplyButton;
+    private Button fourButton;
+    private Button fiveButton;
+    private Button sixButton;
+    private Button addButton;
+    private Button oneButton;
+    private Button twoButton;
+    private Button threeButton;
+    private Button subtractButton;
+    private Button zeroButton;
+    private Button decimalButton;
+    private Button calculateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,27 +41,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        //widget initialization
+        outputTextView = (TextView) findViewById(R.id.outputTextView);
+        clearButton = (Button) findViewById(R.id.clearButton);
+        posNegButton = (Button) findViewById(R.id.posNegButton);
+        percentButton = (Button) findViewById(R.id.percentButton);
+        divideButton = (Button) findViewById(R.id.divideButton);
+        sevenButton = (Button) findViewById(R.id.sevenButton);
+        eightButton = (Button) findViewById(R.id.eightButton);
+        nineButton = (Button) findViewById(R.id.nineButton);
+        multiplyButton = (Button) findViewById(R.id.multiplyButton);
+        fourButton = (Button) findViewById(R.id.fourButton);
+        fiveButton = (Button) findViewById(R.id.fiveButton);
+        sixButton = (Button) findViewById(R.id.sixButton);
+        addButton = (Button) findViewById(R.id.addButton);
+        oneButton = (Button) findViewById(R.id.oneButton);
+        twoButton = (Button) findViewById(R.id.twoButton);
+        threeButton = (Button) findViewById(R.id.threeButton);
+        subtractButton = (Button) findViewById(R.id.subtractButton);
+        zeroButton = (Button) findViewById(R.id.zeroButton);
+        decimalButton = (Button) findViewById(R.id.decimalButton);
+        calculateButton = (Button) findViewById(R.id.calculateButton);
     }
 }
